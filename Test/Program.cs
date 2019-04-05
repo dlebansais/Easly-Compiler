8977e68545d8bd265216fc3c53625d87f634077e
dlebansais <dlebansais@gmail.com>
2019-04-05 11:05:28 +0200

    Added tests.

--- Test/Program.cs
+++ Test/Program.cs
@@ -11,7 +11,7 @@ namespace Test
         static void Main(string[] args)
         {
             Compiler c = new Compiler();
-            c.Compile("../../../coverage_replication.easly");
+            c.Compile("../../../coverage invalid 5.easly");
             //c.Compile("../../../test.easly");
 
             Debug.WriteLine($"{c.ErrorList.Count} error(s).");
