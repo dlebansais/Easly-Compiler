3e6957e8bc46b23f7edfc8e80621b8d59c4d7082
dlebansais <dlebansais@gmail.com>
2019-04-29 11:56:05 +0200

    Removed ISource from IFormalGenericType and ITypedefType

--- Easly-Compiler/CompilerNodes/ObjectType/FormalGenericType.cs
+++ Easly-Compiler/CompilerNodes/ObjectType/FormalGenericType.cs
@@ -8,7 +8,7 @@
     /// <summary>
     /// Compiler-only IFormalGenericType.
     /// </summary>
-    public interface IFormalGenericType : ISource, ICompiledType
+    public interface IFormalGenericType : ICompiledType
     {
         /// <summary>
         /// The generic from which this instance is issued.
@@ -49,97 +49,6 @@
         /// The associated unique type name.
         /// </summary>
         public ITypeName ResolvedTypeName { get; }
-        #endregion
-
-        #region Implementation of ISource
-        /// <summary>
-        /// The parent node, null if root.
-        /// </summary>
-        public ISource ParentSource { get; private set; }
-
-        /// <summary>
-        /// The parent class, null if none.
-        /// </summary>
-        public IClass EmbeddingClass { get; private set; }
-
-        /// <summary>
-        /// The parent feature, null if none.
-        /// </summary>
-        public IFeature EmbeddingFeature { get; private set; }
-
-        /// <summary>
-        /// The parent overload, null if none.
-        /// </summary>
-        public IOverload EmbeddingOverload { get; private set; }
-
-        /// <summary>
-        /// The parent body, null if none.
-        /// </summary>
-        public IBody EmbeddingBody { get; private set; }
-
-        /// <summary>
-        /// The parent assertion, null if none.
-        /// </summary>
-        public IAssertion EmbeddingAssertion { get; private set; }
-
-        /// <summary>
-        /// Initializes parents based on the provided <paramref name="parentSource"/> node.
-        /// </summary>
-        /// <param name="parentSource">The parent node.</param>
-        public virtual void InitializeSource(ISource parentSource)
-        {
-        }
-
-        /// <summary>
-        /// Reset some intermediate results.
-        /// </summary>
-        /// <param name="ruleTemplateList">The list of rule templates that would read the properties to reset.</param>
-        public void Reset(IList<IRuleTemplate> ruleTemplateList)
-        {
-            bool IsHandled = false;
-
-            if (ruleTemplateList == RuleTemplateSet.Identifiers)
-            {
-                IsHandled = true;
-            }
-            else if (ruleTemplateList == RuleTemplateSet.Types)
-            {
-                DiscreteTable = new HashtableEx<IFeatureName, IDiscrete>();
-                FeatureTable = new HashtableEx<IFeatureName, IFeatureInstance>();
-                ExportTable = new HashtableEx<IFeatureName, IHashtableEx<string, IClass>>();
-                ConformanceTable = new HashtableEx<ITypeName, ICompiledType>();
-                InstancingRecordList = new List<TypeInstancingRecord>();
-                OriginatingTypedef = new OnceReference<ITypedef>();
-                IsHandled = true;
-            }
-
-            Debug.Assert(IsHandled);
-        }
-
-        /// <summary>
-        /// Checks if a rule is resolved for this source.
-        /// </summary>
-        /// <param name="ruleTemplateList">The list of rule templates that would read the properties to check.</param>
-        public virtual bool IsResolved(IList<IRuleTemplate> ruleTemplateList)
-        {
-            bool IsResolved = false;
-
-            bool IsHandled = false;
-
-            if (ruleTemplateList == RuleTemplateSet.Identifiers)
-            {
-                IsResolved = false;
-                IsHandled = true;
-            }
-            else if (ruleTemplateList == RuleTemplateSet.Types)
-            {
-                IsResolved = false;
-                IsHandled = true;
-            }
-
-            Debug.Assert(IsHandled);
-            return IsResolved;
-        }
         #endregion
 
         #region Implementation of ICompiledType
