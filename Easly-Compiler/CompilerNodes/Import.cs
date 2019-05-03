ff471c0dd066efbe32fe191a07d07a68811d69d2
dlebansais <dlebansais@gmail.com>
2019-05-03 18:31:04 +0200

    Fixed renames.

--- Easly-Compiler/CompilerNodes/Import.cs
+++ Easly-Compiler/CompilerNodes/Import.cs
@@ -231,12 +231,6 @@ namespace CompilerNode
             foreach (IRename RenameItem in RenameList)
                 Success &= RenameItem.CheckGenericRename(new IHashtableIndex<string>[] { importedClassTable }, SourceIdentifierTable, DestinationIdentifierTable, (string key) => key, (string s) => s, errorList);
 
-            if (Success)
-            {
-                foreach (KeyValuePair<string, string> Entry in SourceIdentifierTable)
-                    importedClassTable.ChangeKey(Entry.Key, Entry.Value);
-            }
-
             Debug.Assert(Success || !errorList.IsEmpty);
             return Success;
         }
